6eb94dfdebf58d3615ba51293c99a77642930484
Tatevik1712 <tatev20050716@gmail.com>
2026-07-23 18:53:11 +0400

    Тестирование

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
 //export const API_BASE_URL = "http://127.0.0.1:8000";
-export const API_BASE_URL = "https://28cb-2a00-f3c-9f5c-0-3d8d-3038-1363-ce0d.ngrok-free.app";
+//export const API_BASE_URL = "https://28cb-2a00-f3c-9f5c-0-3d8d-3038-1363-ce0d.ngrok-free.app";
+export const API_BASE_URL = "https://e566-195-250-89-194.ngrok-free.app";
+// вставлять из 2-го терминала ngrok http 8000
